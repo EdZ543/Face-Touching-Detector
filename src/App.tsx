dbc0af0597c868f74920387621604f5acab0806f
EdZ543 <zhuang.eddie@gmail.com>
2022-06-19 19:05:24 -0400

    Add face touching detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import "./App.css";
 import Webcam from "./components/Webcam";
 
 function App() {
   return (
     <div className="App">
-      <Webcam />
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>Face Touching Detector</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container id="webcam-container">
+        <Webcam />
+      </Container>
     </div>
   );
 }
--- src/components/Webcam.tsx
+++ src/components/Webcam.tsx
@@ -6,10 +6,49 @@ let canvas: HTMLCanvasElement;
 let context: CanvasRenderingContext2D;
 let model: any;
 
+interface Prediction {
+  bbox: number[];
+}
+
+function overlapping(predA: Prediction, predB: Prediction) {
+  // bbox: [x, y, width, height]
+  const bboxA = predA.bbox,
+    bboxB = predB.bbox;
+
+  // If one box is to the left of the other, no overlap
+  if (bboxA[0] > bboxB[0] + bboxB[2] || bboxB[0] > bboxA[0] + bboxA[2])
+    return false;
+
+  // If one box is above the other, no overlap
+  if (bboxA[1] > bboxB[1] + bboxB[3] || bboxB[1] > bboxA[1] + bboxA[3])
+    return false;
+
+  return true;
+}
+
 async function render() {
   const predictions = await model.detect(video);
 
   model.renderPredictions(predictions, canvas, context, video);
+
+  let facePreds = [],
+    handPreds = [];
+
+  for (let i = 0; i < predictions.length; i += 1) {
+    if (predictions[i].label === "face") {
+      facePreds.push(predictions[i]);
+    } else {
+      handPreds.push(predictions[i]);
+    }
+  }
+
+  for (let i = 0; i < facePreds.length; i += 1) {
+    for (let j = 0; j < handPreds.length; j += 1) {
+      if (overlapping(facePreds[i], handPreds[j])) {
+        console.log("Face and hand overlapping");
+      }
+    }
+  }
 
   requestAnimationFrame(render);
 }
