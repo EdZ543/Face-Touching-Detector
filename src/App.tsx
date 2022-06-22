48e592b256c36af32434a332970199c48aec8a85
EdZ543 <zhuang.eddie@gmail.com>
2022-06-21 21:50:02 -0400

    Settings synced using Indexeddb!

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import "./App.css";
 import Webcam from "./components/Webcam";
 import Settings from "./components/Settings";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import * as localForage from "localforage";
 
 function App() {
   const [notifications, setNotifications] = useState(false);
   const [sounds, setSounds] = useState(false);
 
+  useEffect(() => {
+    localForage.getItem("notifications").then((value) => {
+      if (value !== null) setNotifications(value as boolean);
+    });
+
+    localForage.getItem("sounds").then((value) => {
+      if (value !== null) setSounds(value as boolean);
+    });
+  }, [notifications, sounds]);
+
+  function setNotificationsSync(x: boolean) {
+    localForage.setItem("notifications", x);
+    setNotifications(x);
+  }
+
+  function setSoundsSync(x: boolean) {
+    localForage.setItem("sounds", x, (value) => console.log(value));
+    setSounds(x);
+  }
+
   return (
     <div className="flex App">
       <Settings
         sounds={sounds}
-        setSounds={setSounds}
+        setSounds={setSoundsSync}
         notifications={notifications}
-        setNotifications={setNotifications}
+        setNotifications={setNotificationsSync}
       />
       <div className="flex" id="webcam-panel">
         <Webcam notifications={notifications} sounds={sounds} />
